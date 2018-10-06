76f122d483e601007ac56049ccd9d1b47d7b0f08
evanrupert <rupertevanr@gmail.com>
2018-10-06 00:20:50 -0400

    Added error SnackBar to VanityUsernameInput component

--- ui/src/VanityUsernameInput/VanityUsernameInput.js
+++ ui/src/VanityUsernameInput/VanityUsernameInput.js
@@ -1,71 +1,67 @@
 import React, { Component } from 'react';
 import './VanityUsernameInput.css'
-import {Button, TextField, Tooltip} from '@material-ui/core'
-
-const styles = {
-  errorTooltip: {
-    fontSize: 14,
-    background: "#F31431"
-  }
-};
+import {Button, TextField, Snackbar} from '@material-ui/core'
 
 export class VanityUsernameInput extends Component {
   constructor(props) {
     super(props);
     this.state = {
       value: '',
-      error: null
+      error: null,
+      snackBar: false
     };
 
     this.handleTextChange = this.handleTextChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.getTextField = this.getTextField.bind(this);
+    this.handleSnackBarClose = this.handleSnackBarClose.bind(this);
   }
 
   handleTextChange(event) {
-    this.setState({value: event.target.value, error: null});
+    this.setState({value: event.target.value, error: null, snackBar: false});
   }
 
   handleSubmit(event) {
     console.log('handleSubmit: ' + this.state.value);
     let error = this.props.onSubmit(this.state.value);
-    this.setState({value: this.state.value, error: error});
+    this.setState({
+      value: this.state.value,
+      error: error,
+      snackBar: error !== null
+    });
+
     event.preventDefault();
   }
-  
-  getTextField() {
-    let textField =
-      <TextField value={this.state.value}
-                 className="vanityUrlInput"
-                 placeholder="Steam Vanity URL"
-                 onChange={this.handleTextChange}
-                 error={this.state.error !== null}
-      />;
 
-    if (this.state.error === null)
-      return textField;
-
-    return (
-      <Tooltip title={this.state.error}
-               placement="left"
-               classes={{tooltip: styles.errorTooltip}}
-      >
-        {textField}
-      </Tooltip>
-    )
+  handleSnackBarClose() {
+    this.setState({
+      value: this.state.value,
+      error: this.state.value,
+      snackBar: false
+    })
   }
 
   render() {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-          {this.getTextField()}
+          <TextField value={this.state.value}
+                     className="vanityUrlInput"
+                     placeholder="Steam Vanity URL"
+                     onChange={this.handleTextChange}
+                     error={this.state.error !== null}
+          />
           <Button variant="contained"
                   color="primary"
                   onClick={this.handleSubmit}>
             Get Data
           </Button>
         </form>
+        <Snackbar open={this.state.snackBar}
+                  autoHideDuration={2000}
+                  message={this.state.error}
+                  className="errorSnackBar"
+                  onClose={this.handleSnackBarClose}
+        />
       </div>
     );
   }
