1bd39560e4a8cf952bea8a8b78674c815b9fecd5
evanrupert <rupertevanr@gmail.com>
2018-10-07 14:10:48 -0400

    Replaced React frontend with Angular

--- ui/src/DataService.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export function getTagPlaytimeData(username, callback) {
-  let url = '/api/data/' + username;
-  axios.get(url)
-    .then((resp) => callback(resp.data))
-    .catch((resp) => console.log(resp))
-}
