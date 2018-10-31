861580d2ca87845e6a7bedf02847c7fa5b970d3f
Evan Rupert <rupertevanr@gmail.com>
2018-10-31 17:41:54 -0400

    Added material angular

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -6,7 +6,9 @@ import { dataReducer } from './reducers/data.reducer'
 import { AppComponent } from './app.component'
 import { VanityUrlInputComponent } from './vanity-url-input/vanity-url-input.component'
 import { PlaytimeGraphComponent } from './playtime-graph/playtime-graph.component'
-import {ChartsModule} from 'ng2-charts'
+import { ChartsModule } from 'ng2-charts'
+import { MatProgressBarModule } from '@angular/material'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
@@ -16,8 +18,10 @@ import {ChartsModule} from 'ng2-charts'
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     StoreModule.forRoot({ data: dataReducer }),
-    ChartsModule
+    ChartsModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
